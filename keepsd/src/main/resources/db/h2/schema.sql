5c4b1a52b26d3768a103daad3eed110f3bdb0469
gentledot <gentledot.wp@gmail.com>
2020-06-13 20:25:29 +0900

    KeepSD_board - Initialization

--- keepsd/src/main/resources/db/h2/schema.sql
+++ keepsd/src/main/resources/db/h2/schema.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS ksd_board;
-DROP TABLE IF EXISTS tbl_tmp;
 
 CREATE TABLE ksd_board(
     board_no   bigint PRIMARY KEY AUTO_INCREMENT COMMENT '게시물 번호',
@@ -9,8 +8,3 @@ CREATE TABLE ksd_board(
     created_at TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '등록일시',
     updated_at TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시'
 );
-
-CREATE TABLE tbl_tmp(
-    tmpno bigint primary key  AUTO_INCREMENT,
-    tmpstr varchar(100)
-)
